e357fb4a6aba7e2d0daf6a7f647fcdfbf87a521a
An-124Rus <111499611+An-124Rus@users.noreply.github.com>
2022-08-23 22:57:12 +0300

    HomeWork10

---
+++ HomeWork10/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int numberForCount;
+
+        Console.Write("Введите начальное число: ");
+        int minimumNumber = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите конечное число: ");
+        int maximumNumber = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите шаг: ");
+        int step = Convert.ToInt32(Console.ReadLine());
+
+        for (numberForCount = minimumNumber; numberForCount <= maximumNumber; numberForCount += step)
+        {
+            Console.WriteLine(numberForCount);
+        }
+
+        //Выбор цикла обусловлен заданными начальным и конечным значениями
+
+        // Можно компактнее:
+
+        // int numberForCount;
+
+        // for (numberForCount = 5; numberForCount <= 96; numberForCount += 7)
+        // {
+        //     Console.WriteLine(numberForCount);
+        // }
+    }
+}
